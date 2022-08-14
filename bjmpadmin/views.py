23f1a903bfea8e9f3f9984a8c514d4387562364c
k3nmasanguid <k3nmasanguid.v2@gmail.com>
2022-08-14 18:25:28 +0800

    8/14/2022 6.25pm

--- bjmpadmin/views.py
+++ bjmpadmin/views.py
@@ -1,14 +1,20 @@
+
 from django.shortcuts import render
 from authentication.models import User
 from rma.models import College
 from django.db.models import Q
 def bjmpadmin(request):
-    # queryset1  = User.objects.prefetch_related('batch').filter(batch__batch='2024-01')
+    queryset1  = User.objects.prefetch_related('batch').filter(batch__batch='2024-01')
     # results = User.objects.filter(id__in=queryset1).select_related('personalinfo')
     # resultsss = College.objects.filter(user_id=2).select_related('user')
+    infos = User.objects.filter(id__in=queryset1).select_related('personalinfo')
+    
 
-    courses = User.objects.get(id=2)
-    courses.college_set.all()
+
+    context = {
+        'infos':infos,
+       
+    }
     # college_results = College.objects.select_related('user')#.filter(Q(user_id__id=queryset1)& ~Q(year_graduated = 'N/A')).first()
     # # # for x in results:
     # # #     print(x.personalinfo.last_name,x.presentaddress.zip_code)
@@ -19,4 +25,4 @@ def bjmpadmin(request):
     #     courses = College.objects.filter(user_id=x.id).select_related('user')
    
 
-    return render(request, 'bjmpadmin/bjmpadmin.html',)#{'results':results,})
+    return render(request, 'bjmpadmin/bjmpadmin.html',context)#{'results':results,})
